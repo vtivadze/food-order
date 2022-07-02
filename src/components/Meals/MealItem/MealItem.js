04985bb1b4a3357617313105c5372b09c3daeb7b
Vakhtang Tivadze <vaxotivadze@gmail.com>
2022-07-02 11:20:00 +0400

    Create MealItemForm component

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,7 +1,9 @@
+import MealItemForm from './MealItemForm';
 import classes from './MealItem.module.css';
 
 const MealItem = props => {
   const price = `$${props.price.toFixed(2)}`;
+
   return (
     <li className={classes.meal}>
       <div>
@@ -10,7 +12,7 @@ const MealItem = props => {
         <div className={classes.price}>{price}</div>
       </div>
       <div>
-      
+        <MealItemForm id={props.id} />
       </div>
     </li>
   );
