b7265db563ba9824ec207c268726b6afb1d229cb
Vakhtang Tivadze <vaxotivadze@gmail.com>
2022-07-02 12:42:39 +0400

    Implement Context for Cart state management

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import { Fragment, useState } from 'react';
+import { useState } from 'react';
 
+import CartProvider from './store/CartProvider';
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 import Cart from './components/Cart/Cart';
@@ -16,13 +17,13 @@ function App() {
   };
   
   return (
-    <Fragment>
+    <CartProvider>
       {cartIsShown && <Cart onClose={hideCartHandler} />}
       <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
-    </Fragment>
+    </CartProvider>
   );
 }
 
